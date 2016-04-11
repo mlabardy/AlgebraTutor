3eea64129b39a0b196c908fe0702704beec77e09
yfeugueur <yann.feugueur@gmx.fr>
2016-04-11 18:46:36 +0200

    Basic server without file/exercice

---
+++ server/Server.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include "Server.hpp"
+#include <thread>
+ 
+using namespace std;
+
+Server::Server(int port){
+    _port = port;
+    _socketServer = socket(AF_INET, SOCK_STREAM, 0);
+
+    if( _socketServer < 0 ){
+        cerr << "Error establishing socket..." << endl;
+        exit(1);
+    }
+
+    _server_addr.sin_family = AF_INET;
+    _server_addr.sin_addr.s_addr = htons(INADDR_ANY);
+    _server_addr.sin_port = htons(port);
+}
+
+void Server::start(){
+    if ((bind(_socketServer, (struct sockaddr*)&_server_addr,sizeof(_server_addr))) < 0){
+        cerr << "Error binding connection, the socket has already been established..." << endl;
+        exit(1);
+    }
+
+    if( listen(_socketServer, 0) == -1){
+        cerr << strerror(errno) << endl;
+        exit(1);
+    }
+
+    _size = sizeof(_server_addr);
+
+    thread thr(&Server::acceptClient, this );
+    thr.join();
+}
+
+void Server::acceptClient(){
+    int fd;
+    while(_isAccepted){
+        fd = accept(_socketServer, (struct sockaddr *)&_server_addr,&_size);
+
+        send(fd, "Connection established\n", sizeof("Connection established\n"), 0);
+
+        _mutex.lock();
+        _clients.push_back( fd );
+        _mutex.unlock();
+    }
+}
+
+void Server::sendToClient(){
+    _mutex.lock();
+
+    for(vector<int>::iterator it = _clients.begin() ; it < _clients.end(); it++ ) {
+        cout<< *it << endl; 
+    }
+
+    _mutex.unlock();
+}
+
+int main(void){
+    string path;
+    Server *server = new Server(8080);
+
+    server->start();
+
+
+    cout << "Path of file to send at students?" << endl;
+    cin >> path;
+
+    return 0;
+}
---
+++ server/Server.hpp
@@ -0,0 +1,40 @@
+#ifndef __SERVER_HPP__
+#define __SERVER_HPP__
+
+#include <iostream>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <thread>
+#include <vector>
+#include <mutex>
+ 
+using namespace std;
+
+class Server{
+	private:
+		int _socketServer;
+		int _client;
+		int _port;
+		bool _isAccepted = true;
+		vector<int> _clients;
+		mutex _mutex;
+		//bool isExit = false;
+		//int bufsize = 1024;
+		//char buffer[bufsize];
+		 
+		struct sockaddr_in _server_addr;
+		socklen_t _size;
+	public:
+		Server(int port);
+		~Server();
+		void start();
+		void acceptClient();	
+		void sendToClient();
+};
+#endif
